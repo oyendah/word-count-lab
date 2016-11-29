151f478d96fbbf0ef501f774fef413254696caa9
Subair Oyindamola <hoyendah@live.co.uk>
2016-11-29 09:40:58 +0100

    Added comments

--- app/library.js
+++ app/library.js
@@ -1,10 +1,11 @@
 module.exports = function(s){
-    s = s.replace(/(^\s*)|(\s*$)/gi,"");//exclude  start and end white-space
-    s = s.replace(/[ ]{2,}/gi," ");//2 or more space to 1
-    s = s.replace(/\n /,"\n");
+    s = s.replace(/(^\s*)|(\s*$)/gi,""); //exclude  start and end white-space
+    s = s.replace(/[ ]{2,}/gi," "); //2 or more space to 1
+    s = s.replace(/\n\r/,"\n"); //newline
 	var words = s.split(/\s/);
     var wordMap = {};
     words.forEach(function(w) {
+        //check if a word has its own property
         if(Object.hasOwnProperty.call(wordMap, w)){
             wordMap[w] += 1;
         }
